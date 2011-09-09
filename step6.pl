e8400a65e2c04cabe886132e9b8e102da8fcb511
parkerl <parker@earthvectors.com>
2011-09-09 10:11:48 -0600

    step 7

--- step6.pl
+++ step6.pl
@@ -2,19 +2,28 @@
 
 % defeine rules for each type of caculation
 
+factorial(0,1). 
+
+factorial(N,F) :-  
+   N>0, 
+   N1 is N-1, 
+   factorial(N1,F1), 
+   F is N * F1.
+
 calc(Num1,Op,Result) :- Op == +,  write('Number: '), read(Num2), Result is Num1 + Num2.
 calc(Num1,Op,Result) :- Op == -,  write('Number: '), read(Num2),Result is Num1 - Num2. 
 calc(Num1,Op,Result) :- Op == /,  write('Number: '), read(Num2),Result is Num1 / Num2. 
 calc(Num1,Op,Result) :- Op == *,  write('Number: '), read(Num2),Result is Num1 * Num2. 
 calc(Num1,Op,Result) :- Op == ^,  write('Number: '), read(Num2),Result is Num1 ^ Num2. 
 calc(Num1,Op,Result) :- Op == sq, Result is Num1 ^ 2. 
-calc(_,_,'Unknown operation').
-
-% define a predicate that takes 3 arguments, two numbers and an operator
-
-%define a predicate that asks for user input
-% if input = "=" then print the result else ask for a number. 
-% then calculate.
+calc(Num1,Op,Result) :- Op == cu, Result is Num1 ^ 3. 
+calc(Num1,Op,Result) :- Op == sqrt, Result is sqrt(Num1). 
+calc(Num1,Op,Result) :- Op == sin, Result is sin(Num1). 
+calc(Num1,Op,Result) :- Op == tan, Result is tan(Num1). 
+calc(Num1,Op,Result) :- Op == cos, Result is cos(Num1). 
+calc(Num1,Op,Result) :- Op == log, Result is log(Num1). 
+calc(Num1,Op,Result) :- Op == fact, factorial(Num1,F), Result is F. 
+calc(Num1,_,Result) :- write('Unknown operation'),nl, Result is Num1.
 
 calcloop(Num1) :-
 write('Operation: '), read(Op),
---
+++ step7.pl
@@ -0,0 +1,33 @@
+% Printing output in different formats (say rounding up to 5 decimal places, truncating after 
+% 4 decimal places, padding zeros to the right and left, right and left justification)(Input output operations) 
+
+round_to(X,Places,Result) :-
+Y is float_fractional_part(X),
+atom_number(A,Y),
+Trim is Places + 2,
+sub_atom(A,0,Trim,C, _),
+C == 0,
+Result=X.
+
+
+
+round_to(X,Places,Result) :-
+Y is float_fractional_part(X),
+Z is integer(float_integer_part(X)),
+atom_number(A,Y),
+Trim is Places + 3,
+(sub_atom(A,0,Trim,_, Num),atom_number(Num, Fract),
+Final is round(Fract * (10^Places))/(10^Places),
+Result is Z + Final;Result = X).
+
+
+
+pad(X,Pad,Result) :-
+atom_length(X,Len),
+(Len==Pad,Result=X;
+atom_chars(X,L),
+append(L,['0'],L2),
+concat_atom(L2,X3),
+pad(X3,Pad,Result)
+)
+
